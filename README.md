61e1c455928380778d0455d140d047eab8750ff2
hvianna <hvianna@gmail.com>
2018-12-04 20:50:44 -0200

    First commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+perfTest.js
+===========
+
+a simple platform to test the performance of JavaScript code
+
+https://hvianna.github.io/perfTest.js/
+
+## About
+
+I wrote this for my own use. It's in a very initial stage. Someone else may find it useful though. Or not.
+
+## Acknowledgements
+
+perfTest.js uses [CodeMirror](https://codemirror.net) for in-browser code editing.
+CodeMirror copyright (c) 2017 by Marijn Haverbeke and others
+Distributed under an MIT license: https://codemirror.net/LICENSE
+
+## License
+
+perfTest.js copyright (c) 2018 Henrique Vianna
+Licensed under the MIT License: https://github.com/hvianna/perfTest.js/blob/master/LICENSE
---
+++ perftest.js
@@ -0,0 +1,138 @@
+/**
+ * perfTest.js
+ * a simple platform to test the performance of JavaScript code
+ *
+ * Copyright (c) 2018 Henrique Vianna
+ * Licensed under the MIT License: https://github.com/hvianna/perfTest.js/blob/master/LICENSE
+ */
+
+
+/**
+ * Global variables
+ */
+var n = 0,
+	averages = [0, 0, 0],
+	running = false,
+	editor = [],
+	editor_init;
+
+
+/**
+ * Reset run history
+ */
+function reset() {
+	n = 0;
+	averages = [0, 0, 0];
+
+	for ( var i = 0; i < 3; i++ )
+		document.getElementById(`function${i}_result`).innerHTML = '';
+}
+	
+
+/**
+ * Run the tests
+ */
+function do_it() {
+
+	if ( running )
+		return false;
+
+	running = true;
+
+	document.getElementById('start_button').value=" WAIT... ";
+
+	n++;
+
+	var userFunction = [];
+
+	var outputDiv = [
+			document.getElementById('function0_result'),
+			document.getElementById('function1_result'),
+			document.getElementById('function2_result')
+	]
+
+	var start, now, elapsed, repetitions, target, refTime, className;
+
+	var i = 0;
+
+	editor_init.save();
+	eval( document.getElementById('initialize').value.trim() );
+
+	editor[0].save();
+	userFunction[0] = document.getElementById('function0').value.trim();
+
+	start = new Date() | 0;
+	repetitions = 0;
+
+	do {
+		eval( userFunction[ i ] );
+		repetitions++;
+
+		now = new Date() | 0;
+		elapsed = ( now - start ) / 1000;
+	}
+	while ( elapsed < 1 );
+
+	refTime = elapsed;
+	averages[0] += repetitions;
+
+	outputDiv[0].innerHTML = '<span class="label">Last run</span>' + repetitions + ' repetitions in <strong>' + elapsed + 's</strong>' +
+							 '<span class="label">Average of ' + n + ' runs</span>' + Math.round( averages[0] / n ) + ' repetitions';
+
+	target = repetitions;
+
+	for ( i = 1; i < 3; i++ ) {
+
+		editor[ i ].save();
+		userFunction[ i ] = document.getElementById(`function${i}`).value.trim();
+
+		if ( userFunction[ i ] ) {
+
+			start = new Date() | 0;
+			repetitions = 0;
+
+			do {
+				eval( userFunction[ i ] );
+				repetitions++;
+
+				now = new Date() | 0;
+				elapsed = ( now - start ) / 1000;
+			}
+			while ( repetitions < target );
+
+			averages[ i ] += elapsed / refTime - 1;
+
+			outputDiv[ i ].innerHTML = '<span class="label">Last run</span>' + repetitions + ' repetitions in <strong>' + elapsed + 's</strong>' +
+									   '<span class="label">Average of ' + n + ' runs</span>';
+
+			ratio = parseInt( averages[ i ] / n * 10000 ) / 100;
+
+			outputDiv[ i ].innerHTML += '<span class="' + ( className = ( ratio < 0 ? 'faster' : 'slower' ) ) + '">' + Math.abs( ratio ) + '% ' + className;
+		}
+	}
+
+	running = false;
+
+	document.getElementById('start_button').value=" RUN IT! ";
+
+}
+
+
+/**
+ * Initialize
+ */
+function initialize() {
+
+	editor_init = CodeMirror.fromTextArea( document.getElementById('initialize'), {
+//		lineNumbers: true,
+		mode: 'javascript'
+	});
+
+  	for ( var i = 0; i < 3; i++ ) {
+		editor[ i ] = CodeMirror.fromTextArea( document.getElementById(`function${i}`), {
+			mode: 'javascript'
+		});
+  	}
+}
+
+window.onload = initialize;
